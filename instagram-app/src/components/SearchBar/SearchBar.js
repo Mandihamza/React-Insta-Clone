bb7fa20730cffad0b7c72a2e4c34348f6955f554
mandihamza <mandihamza@gmail.com>
2019-05-25 11:10:18 -0600

    Built out file structure for components

---
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SearchBar.css'
+
+function SearchBar(props) {
+    return (
+        <div className='search-container'> <p>React!</p></div>
+    );
+}
+
+export default SearchBar;
