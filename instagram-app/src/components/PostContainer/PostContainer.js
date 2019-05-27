9c98b90e50bfee66ab6796fb0a2b17639c4f8b55
mandihamza <mandihamza@gmail.com>
2019-05-27 13:15:24 -0600

    Completed styling for search bar component

---
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './PostContainer.css'
+
+function PostContainer(props) {
+    return (
+        <div className='post-container'> <p>Post Container</p></div>
+    );
+}
+
+export default PostContainer;
--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import './SearchBar.css'
+import instalogo from './instalogo.svg';
 
 function SearchBar(props) {
     return (
-        <div className='search-container'> <p>React!</p></div>
+        <div className='search-bar-container'>
+            <div className="left-side-header">
+            <i class="fab fa-instagram fa-2x"></i>
+            <img className="text-logo" src={ instalogo } alt="instagram logo" />
+            </div>
+
+            <div className="search-wrapper">
+            <form>
+                <input class="search-box" type="text" placeholder=" Search" />
+            </form>
+            </div>
+            
+            <div className="right-side-header">
+                <i class="far fa-compass fa-lg"></i>
+                <i class="far fa-heart fa-lg"></i>
+                <i class="far fa-user fa-lg"></i>
+            </div>
+            
+        </div> 
     );
 }
 
