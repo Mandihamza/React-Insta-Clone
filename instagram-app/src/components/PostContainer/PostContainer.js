fa6050f47e56bbaf1107658bd1205324ca09af97
mandihamza <mandihamza@gmail.com>
2019-05-28 20:53:00 -0600

    Added styling for comment section component

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import './PostContainer.css'
+import UploadIcon from './UploadIcon';
+
+
 
 function PostContainer(props) {
     return (
-        <div className='post-container'> <p>Post Container</p></div>
+        <div className='post-container'> 
+            
+            <div className="post-header">
+                <img src="https://source.unsplash.com/random/1" alt="Profile"/>
+                <h1>Display Name</h1>
+            </div>
+            
+            <div className="post-img">
+                <img src="https://source.unsplash.com/random/2" alt="Post img"/>
+            </div>
+            
+            <div className="post-social-icons">
+                <i class="far fa-heart fa-lg" />
+                <i class="far fa-comment fa-flip-horizontal fa-lg" />
+                <UploadIcon />
+            </div>
+
+            <div className="post-interactions">
+                <div className="likes-count">
+                    <p>369</p>
+                </div>
+                <p>likes</p>
+            </div>
+        </div>
     );
 }
 
